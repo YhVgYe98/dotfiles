bf7248e449044e999d11137b4a735e5ac6cc5180
YhVgYe98 <zhengye_yang@qq.com>
2024-07-02 23:40:07 +0800

    Update start.bat

--- aria2/start.bat
+++ aria2/start.bat
@@ -2,7 +2,7 @@
 
 tasklist | findstr /i aria2c.exe
 if errorlevel 1 (
-    aria2c --conf-path path\to\config\file --input-file path\to\session\file
+    aria2c --conf-path path\to\config\file --input-file path\to\session\file --all-proxy="http://127.0.0.1:7890"
 )
 else (
     
